6c80a4754f9afe0e705c9b9cf3afe23c5d717c6f
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-09-13 08:33:10 +0700

    Task_1_1_2 new branch

---
+++ Task_1_1_2/src/main/java/ru/nsu/kotenkov/polynomial/Polynomial.java
@@ -0,0 +1,84 @@
+package ru.nsu.kotenkov.polynomial;
+
+public class Polynomial {
+    public int[] cfs;
+    public int degree;
+
+    public Polynomial(int[] parameters) {
+        degree = parameters.length;
+        cfs = parameters;
+    }
+
+    public int evaluate(int val) {
+        int res = 0;
+
+        for (int i = 0; i < this.degree; i++) {
+            res += (int) (this.cfs[i] * Math.pow(val, this.degree - i - 1));
+        }
+
+        return res;
+    }
+
+    public int[] plus(Polynomial p2) {
+        int[] new_cfs;
+
+        if (this.degree < p2.degree) {
+            new_cfs = p2.cfs;
+            for (int i = 0; i < this.degree; i++) {
+                new_cfs[p2.degree - i - 1] += this.cfs[this.degree - i - 1];
+            }
+            this.degree = p2.degree;
+        } else {
+            new_cfs = this.cfs;
+            for (int i = 0; i < p2.degree; i++) {
+                new_cfs[this.degree - i - 1] += p2.cfs[p2.degree - i - 1];
+            }
+        }
+
+        this.cfs = new_cfs;
+
+        return this.cfs;
+    }
+
+    public int[] minus(Polynomial p2) {
+        int[] new_cfs;
+
+        if (this.degree < p2.degree) {
+            new_cfs = p2.cfs;
+            for (int i = 0; i < this.degree; i++) {
+                new_cfs[p2.degree - i - 1] -= this.cfs[this.degree - i - 1];
+            }
+            this.degree = p2.degree;
+        } else {
+            new_cfs = this.cfs;
+            for (int i = 0; i < p2.degree; i++) {
+                new_cfs[this.degree - i - 1] -= p2.cfs[p2.degree - i - 1];
+            }
+        }
+
+        this.cfs = new_cfs;
+
+        return this.cfs;
+    }
+
+    public int[] times(Polynomial p2) {
+        int[] new_cfs;
+
+        if (this.degree < p2.degree) {
+            new_cfs = p2.cfs;
+            for (int i = 0; i < this.degree; i++) {
+                new_cfs[p2.degree - i - 1] *= this.cfs[this.degree - i - 1];
+            }
+            this.degree = p2.degree;
+        } else {
+            new_cfs = this.cfs;
+            for (int i = 0; i < p2.degree; i++) {
+                new_cfs[this.degree - i - 1] *= p2.cfs[p2.degree - i - 1];
+            }
+        }
+
+        this.cfs = new_cfs;
+
+        return this.cfs;
+    }
+}
---
+++ Task_1_1_2/src/test/java/ru/nsu/kotenkov/polynomial/PolynomialTest.java
@@ -0,0 +1,81 @@
+package ru.nsu.kotenkov.polynomial;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PolynomialTest {
+    @Test
+    @DisplayName("Init test")
+    void checkInit() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        assertArrayEquals(new int[] {4, 3, 6, 7}, p1.cfs);
+    }
+
+    @Test
+    void checkDegree() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        if (p1.degree == 4) {
+            assertTrue(true);
+        } else {
+            fail("Not correct degree");
+        }
+    }
+
+    @Test
+    void checkEval() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        if (p1.evaluate(3) == 160) {
+            assertTrue(true);
+        } else {
+            fail("Wrong evaluate result");
+        }
+    }
+
+    @Test
+    void checkSum_p1SmallOrEqual() {
+        Polynomial p1 = new Polynomial(new int[] {2, 3});
+        Polynomial p2 = new Polynomial(new int[] {4, 3, 6, 7});
+        assertArrayEquals(new int[] {4, 3, 8, 10}, p1.plus(p2));
+    }
+
+    @Test
+    void checkSum_p2Small() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {1, 2, 3});
+        p1.minus(p2);
+        assertArrayEquals(new int[] {4, 2, 4, 4}, p1.cfs);
+    }
+
+    @Test
+    void checkSub_p1SmallOrEqual() {
+        Polynomial p1 = new Polynomial(new int[] {2, 3});
+        Polynomial p2 = new Polynomial(new int[] {4, 3, 6, 7});
+        assertArrayEquals(new int[] {4, 3, 4, 4}, p1.minus(p2));
+    }
+
+    @Test
+    void checkSub_p2Small() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {1, 2, 3});
+        p1.minus(p2);
+        assertArrayEquals(new int[] {4, 2, 4, 4}, p1.cfs);
+    }
+
+    @Test
+    void checkMul_p1SmallOrEqual() {
+        Polynomial p1 = new Polynomial(new int[] {2, 3});
+        Polynomial p2 = new Polynomial(new int[] {4, 3, 6, 7});
+        assertArrayEquals(new int[] {4, 3, 12, 21}, p1.times(p2));
+    }
+
+    @Test
+    void checkMul_p2Small() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[] {1, 2, 3});
+        p1.times(p2);
+        assertArrayEquals(new int[] {4, 3, 12, 21}, p1.cfs);
+    }
+}
