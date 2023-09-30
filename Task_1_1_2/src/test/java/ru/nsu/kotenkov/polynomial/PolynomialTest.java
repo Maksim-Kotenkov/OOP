145d490b72c624c9a9c3c45e8c62f84e351840d7
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-09-30 11:27:44 +0700

    deleted extra line before another import...

--- Task_1_1_2/src/test/java/ru/nsu/kotenkov/polynomial/PolynomialTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kotenkov/polynomial/PolynomialTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
