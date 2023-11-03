f3e536ae660e563d692472f2db1bf55e69139594
Maksim Kotenkov <59960529+Maksim-Kotenkov@users.noreply.github.com>
2023-11-03 18:10:55 +0700

    ONE WARNING FIX

--- Task_1_2_2/src/test/java/ru/nsu/kotenkov/graph/GraphTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/kotenkov/graph/GraphTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
