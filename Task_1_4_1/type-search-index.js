499597f80540ee8d3b69a6c4220a53ecfeed2bb1
Maksim-Kotenkov <Maksim-Kotenkov@users.noreply.github.com>
2023-11-25 04:24:52 +0000

    Deploying to gh-pages from @ Maksim-Kotenkov/OOP@977d95ea0c20ecdd6226eed9c04334a008724201 🚀

--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kotenkov.gradebook","l":"GradeBook"},{"p":"ru.nsu.kotenkov.gradebook","l":"IncorrectMarkException"},{"p":"ru.nsu.kotenkov.gradebook","l":"IncorrectSemesterException"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kotenkov.gradebook","l":"GradeBook"},{"p":"ru.nsu.kotenkov.gradebook","l":"IncorrectSemesterException"},{"p":"ru.nsu.kotenkov.gradebook","l":"GradeBook.Mark"}]
