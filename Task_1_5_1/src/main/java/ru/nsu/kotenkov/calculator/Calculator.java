46d7abd802ebfb1212f32d959833a8659f478169
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-12-01 12:42:18 +0700

    Solving the prompt with enums for operations types

--- Task_1_5_1/src/main/java/ru/nsu/kotenkov/calculator/Calculator.java
+++ Task_1_5_1/src/main/java/ru/nsu/kotenkov/calculator/Calculator.java
@@ -1,14 +1,69 @@
 package ru.nsu.kotenkov.calculator;
 
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 
 public class Calculator {
 
+    private enum Operation {
+        SIN(1),
+        COS(1),
+        POW(2),
+        PLUS(2),
+        MINUS(2),
+        MUL(2),
+        DIV(2);
+
+        private final int val;
+
+        /**
+         * Constructor to store operations with their valences.
+         *
+         * @param i the valence that is assigned to the enum value
+         */
+        Operation(int i) {
+            this.val = i;
+        }
+
+        /**
+         * Getter for valences of operation.
+         *
+         * @return int
+         */
+        public int getValence() {
+            return val;
+        }
+
+        public double apply(List<Double> args) {
+            return switch (this) {
+                case PLUS -> calcPlus(args);
+                case MINUS -> calcMinus(args);
+                case MUL -> calcMul(args);
+                case DIV -> calcDiv(args);
+                default -> 0;
+            };
+        }
+
+        private double calcPlus(List<Double> args) {
+            return args.get(0) + args.get(1);
+        }
+
+        private double calcMinus(List<Double> args) {
+            return args.get(0) - args.get(1);
+        }
+
+        private double calcMul(List<Double> args) {
+            return args.get(0) * args.get(1);
+        }
+
+        private double calcDiv(List<Double> args) {
+            return args.get(0) / args.get(1);
+        }
+    }
+
     private Stack<Object> operationStack;
+
     public void run() {
         Scanner inpScan = new Scanner(System.in);
         String prompt = inpScan.nextLine();
@@ -19,9 +74,27 @@ public class Calculator {
     private double solve(String prompt) {
         this.operationStack = eval(prompt);
 
-        System.out.println(operationStack);
+//        System.out.println(operationStack);
 
-        return 0;
+        Stack<Double> numStack = new Stack<>();
+
+        while (!operationStack.empty()) {
+            Object stackObj = operationStack.pop();
+
+            if (stackObj.getClass() == Double.class) {
+                numStack.add((Double) stackObj);
+            } else {
+                Operation currOperation = (Operation) stackObj;
+                List<Double> args = new ArrayList<>();
+                for (int i = 0; i < currOperation.getValence(); i++) {
+                    args.add(numStack.pop());
+                }
+
+                numStack.add(currOperation.apply(args));
+            }
+        }
+
+        return numStack.pop();
     }
 
     private Stack<Object> eval(String prompt) {
@@ -52,11 +125,11 @@ public class Calculator {
                 prompt = prompt.substring(1);
             }
 
-            String command = evalAnything.toString();
-            if (checkCommand(command)) {
+            Operation command = evalOperation(evalAnything.toString());
+            if (command != null) {
                 newStack.add(command);
             } else {
-                throw new WrongCommandException(command);
+                throw new WrongCommandException(evalAnything.toString());
             }
         }
 
@@ -73,11 +146,16 @@ public class Calculator {
         return true;
     }
 
-    private boolean checkCommand(String command) {
-        return command.equals("sin")
-                || command.equals("+")
-                || command.equals("-")
-                || command.equals("*")
-                || command.equals("/");
+    private Operation evalOperation(String command) {
+        return switch (command) {
+            case "+" -> Operation.PLUS;
+            case "-" -> Operation.MINUS;
+            case "*" -> Operation.MUL;
+            case "/" -> Operation.DIV;
+            case "sin" -> Operation.SIN;
+            case "cos" -> Operation.COS;
+            case "pow" -> Operation.POW;
+            default -> null;
+        };
     }
 }
