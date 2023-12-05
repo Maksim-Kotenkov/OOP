cdb0842deed97af384ed1dafb8d8e696c17c11fc
Maksim-Kotenkov <Maksim-Kotenkov@users.noreply.github.com>
2023-12-05 08:59:27 +0000

    Deploying to gh-pages from @ Maksim-Kotenkov/OOP@0cf3c0ef51c812757d0ddbc14d86bff997d9651f 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kotenkov.calculator"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kotenkov.calculator"},{"l":"ru.nsu.kotenkov.calculator.exceptions"}];updateSearchResults();
