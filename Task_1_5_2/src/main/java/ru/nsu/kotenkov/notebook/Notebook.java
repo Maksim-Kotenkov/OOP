e0347a1a301602e164fc3c5714d31f4f05cc3001
Maksim Kotenkov <maximkosmos5360@gmail.com>
2023-12-15 10:19:02 +0700

    dependencies for jackson and args4j

---
+++ Task_1_5_2/src/main/java/ru/nsu/kotenkov/notebook/Notebook.java
@@ -0,0 +1,12 @@
+package ru.nsu.kotenkov.notebook;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.kohsuke.args4j.Argument;
+
+
+public class Notebook {
+    public static void main(String[] args) {
+        
+    }
+}
