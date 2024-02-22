f97b3ac8ec57a491ce66c0f56883a4612ce0faec
Maksim Kotenkov <maximkosmos5360@gmail.com>
2024-02-22 09:01:54 +0700

    backup

--- Task_2_2_1/src/main/java/ru/nsu/kotenkov/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/kotenkov/Main.java
@@ -1,7 +1,21 @@
 package ru.nsu.kotenkov;
 
+import ru.nsu.kotenkov.bakery.Baker;
+import ru.nsu.kotenkov.bakery.Bakery;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            Bakery bakery = new Bakery();
+            for (Baker baker : bakery.getBakers()) {
+                System.out.println(baker.getEfficiency());
+            }
+        } catch (IOException exception) {
+            System.err.println("IOExceptions occurred during the run");
+        }
+
     }
 }
--- Task_2_2_1/src/main/java/ru/nsu/kotenkov/bakery/Bakery.java
+++ Task_2_2_1/src/main/java/ru/nsu/kotenkov/bakery/Bakery.java
@@ -1,4 +1,39 @@
 package ru.nsu.kotenkov.bakery;
 
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Bakery {
+    private List<Baker> bakers;
+    private List<Courier> couriers;
+
+
+    public Bakery() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        File json = Paths.get("config.json").toFile();
+
+        // I need to get a big Map with all parameters and then initialize lists
+        this.bakers = new ArrayList<>(Arrays.asList(
+                mapper.readValue(json, Baker[].class)
+        ));
+    }
+
+    public List<Baker> getBakers() {
+        return bakers;
+    }
+
+    public List<Courier> getCouriers() {
+        return couriers;
+    }
 }
